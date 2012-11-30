a256dc4e6a7d1a9d5cbac988f7fa93c3cdc8e0e0
PDAhubbard <PDAhubbard@gmail.com>
2012-11-30 13:52:13 +0100

    first cpp commit

---
+++ HelloWorld/HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : PDAHubbard
+// Version     :
+// Copyright   : Free for use without restrictions
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
