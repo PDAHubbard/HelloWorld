b72de91a514e43f806db3df5cd3392b6ca1299bd
Peter DA Hubbard <PDAHubbard@gmail.com>
2013-04-05 22:59:05 +0200

    added select queries to lisp

--- make-cd.lisp
+++ make-cd.lisp
@@ -41,3 +41,25 @@
   (with-open-file (in filename)
     (with-standard-io-syntax
       (setf *db* (read in)))))
+
+(defun select-by-artist (artist)
+  (remove-if-not 
+    #'(lambda (cd) (equal (getf cd :artist) artist))
+    *db* ))
+
+(defun artist-selector (artist)
+  #'(lambda (cd) (equal (getf cd :artist) artist)))
+
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
+
+(defun where (&key title artist rating (ripped nil ripped-p))
+  ;; ripped-p otherwise leaving it out would query against ripped==nil
+  #'(lambda (cd)
+    (and
+      (if title     (equal (getf cd :title)   title)    t)
+      (if artist    (equal (getf cd :artist)  artist)   t)
+      (if rating    (equal (getf cd :rating)  rating)   t)
+      (if ripped-p  (equal (getf cd :ripped)  ripped)   t))))
+
+
